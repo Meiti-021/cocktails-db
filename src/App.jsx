6c17e35c682f5cc3ca4e449dc58a5ba6b75c04be
Mahdi <meiti.edu@gmail.com>
2022-12-14 15:57:58 +0330

    create home page placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./Home";
 import Nav from "./navbar";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Nav />
         <Routes>
-          <Route path="/" element={<>i'm home</>} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<>i'm about</>} />
         </Routes>
       </Router>
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.css";
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>I'm HOme</h1>
+    </div>
+  );
+};
+
+export default Home;
