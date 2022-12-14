e0404d834fefd0419b39e87090bce953bd2359ac
Mahdi <meiti.edu@gmail.com>
2022-12-14 12:56:18 +0330

    create navbar component placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<>i'm home</>} />
+          <Route path="/about" element={<>i'm about</>} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
